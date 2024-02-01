9074fa51a7e40a8502a23fe0f126328a843e78fb
harold dinglasan <harolddinglasan90@gmail.com>
2024-02-01 21:55:39 +0800

    added Dinglasan Problem Six

---
+++ Dinglasan Problem Six/src/FibonacciSeries.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.print("Fibonacci: ");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(fibonacci(i) + ", ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+        else
+            return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
