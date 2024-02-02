408bf629c8c4133106eb64ecabf607561edf132b
Roselle <rosellemarithe.mariano@gmail.com>
2024-02-02 21:01:42 +0800

    added exponent by mariano

---
+++ exponent/Exponent.java
@@ -0,0 +1,20 @@
+package com.roselle;
+import java.util.Scanner;
+
+class Exponent{
+	public static void main(String[] args){
+	Scanner sc = new Scanner(System.in);
+
+	int base,exponent;
+	System.out.print("Enter base: ");
+	base = sc.nextInt();
+	System.out.print("Enter exponent: ");
+	exponent = sc.nextInt();
+
+	int result = 1;
+	for(int i = 0; i<exponent; i++){
+	result = result *base;
+	}
+	System.out.print(base + " raised to " + exponent + " is " + result);
+}
+}
