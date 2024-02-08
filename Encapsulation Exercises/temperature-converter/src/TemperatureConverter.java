70c62804ca4f004e1780bd75c946fd8eabe16e19
unknown <vincesarmiento63@gmail.com>
2024-02-08 08:10:32 +0800

    add tempconverter

--- Encapsulation Exercises/temperature-converter/src/TemperatureConverter.java
+++ Encapsulation Exercises/temperature-converter/src/TemperatureConverter.java
@@ -1,19 +1,32 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
+
     public static void main(String[] args) {
-        // Example usage of Temperature class through TemperatureConverter
-        Temperature temperature = new Temperature(32.0);
+        Scanner scanner = new Scanner(System.in);
 
-        double celsiusValue = temperature.getCelsius();
-        double fahrenheitValue = temperature.celsiusToFahrenheit();
+        System.out.print("Enter the temperature value: ");
+        double temperatureValue = scanner.nextDouble();
 
-        System.out.println(celsiusValue + " degrees Celsius is equal to " + fahrenheitValue + " degrees Fahrenheit");
+        System.out.print("Enter the current temperature unit (C for Celsius, F for Fahrenheit): ");
+        char currentUnit = scanner.next().charAt(0);
 
-        temperature.setCelsius(67.0);
+        Temperature_Converter temperatureConverter;
 
-        celsiusValue = temperature.getCelsius();
-        double newFahrenheitValue = temperature.celsiusToFahrenheit();
+        if (currentUnit == 'C' || currentUnit == 'c') {
+            temperatureConverter = new Temperature_Converter(temperatureValue);
+        } else if (currentUnit == 'F' || currentUnit == 'f') {
+            temperatureConverter = new Temperature_Converter();
+            temperatureConverter.setFahrenheit(temperatureValue);
+        } else {
+            System.out.println("Invalid temperature unit. Please enter C or F.");
+            return;
+        }
 
-        System.out.println(celsiusValue + " degrees Celsius is equal to " + newFahrenheitValue + " degrees Fahrenheit");
+        double celsiusValue = temperatureConverter.getCelsius();
+        double fahrenheitValue = temperatureConverter.getFahrenheit();
+
+        System.out.println("Entered temperature: " + temperatureValue + " " + (currentUnit == 'C' || currentUnit == 'c' ? "Celsius" : "Fahrenheit"));
+        System.out.println("Converted temperature: " + celsiusValue + " Celsius is equal to " + fahrenheitValue + " Fahrenheit");
     }
-
 }
