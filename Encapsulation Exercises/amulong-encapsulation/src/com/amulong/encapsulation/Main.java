8ad8dd4238ff071a0f0f8f4a217e35be17f52727
Kate Ann I. Amulong <amulongkateann@gmail.com>
2024-02-08 21:00:03 +0800

    Added Amulong Exercise

---
+++ Encapsulation Exercises/amulong-encapsulation/src/com/amulong/encapsulation/Main.java
@@ -0,0 +1,85 @@
+package com.amulong.encapsulation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CurrencyConverter converter = new CurrencyConverter();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the amount: ");
+        double amount = scanner.nextDouble();
+        converter.setValue(amount);
+
+        System.out.println("Choose the currency to convert from: ");
+        System.out.println("1. EURO");
+        System.out.println("2. USD");
+        System.out.println("3. BAHT");
+        System.out.println("4. JPY");
+        System.out.println("5. WON");
+        System.out.print("Enter your choice: ");
+        int fromCurrency = scanner.nextInt();
+
+        switch (fromCurrency) {
+            case 1:
+                converter.setCurrentCurrency("EURO");
+                break;
+            case 2:
+                converter.setCurrentCurrency("USD");
+                break;
+            case 3:
+                converter.setCurrentCurrency("BAHT");
+                break;
+            case 4:
+                converter.setCurrentCurrency("JPY");
+                break;
+            case 5:
+                converter.setCurrentCurrency("WON");
+                break;
+            default:
+                System.out.println("Invalid choice for currency to convert from!");
+                return;
+        }
+
+        System.out.println("Choose the currency to convert to: ");
+        System.out.println("1. EURO");
+        System.out.println("2. USD");
+        System.out.println("3. BAHT");
+        System.out.println("4. JPY");
+        System.out.println("5. WON");
+        System.out.print("Enter your choice: ");
+        int toCurrency = scanner.nextInt();
+
+        double convertedAmount = 0.0;
+        String targetCurrency = "";
+
+        switch (toCurrency) {
+            case 1:
+                convertedAmount = converter.convertToEuro();
+                targetCurrency = "EURO";
+                break;
+            case 2:
+                convertedAmount = converter.convertToUSD();
+                targetCurrency = "USD";
+                break;
+            case 3:
+                convertedAmount = converter.convertToBaht();
+                targetCurrency = "BAHT";
+                break;
+            case 4:
+                convertedAmount = converter.convertToJPY();
+                targetCurrency = "JPY";
+                break;
+            case 5:
+                convertedAmount = converter.convertToWon();
+                targetCurrency = "WON";
+                break;
+            default:
+                System.out.println("Invalid choice for currency to convert to!");
+                return;
+        }
+
+        System.out.printf("%.2f %s is equivalent to %.2f %s\n", amount, converter.getCurrentCurrency(), convertedAmount, targetCurrency);
+    }
+}
