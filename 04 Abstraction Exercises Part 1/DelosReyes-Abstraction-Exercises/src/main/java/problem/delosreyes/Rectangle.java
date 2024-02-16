af5ca1f263db287fcd9169bb819b499bf7e6f693
daiahnadine <157860737+daiahnadine@users.noreply.github.com>
2024-02-16 17:56:37 +0800

    Added Delos Reyes Abstraction Exercise

---
+++ 04 Abstraction Exercises Part 1/DelosReyes-Abstraction-Exercises/src/main/java/problem/delosreyes/Rectangle.java
@@ -0,0 +1,34 @@
+package main.java.problem.delosreyes;
+
+public class Rectangle {
+    private float length;
+    private float width;
+    private int choice;
+
+    public Rectangle () {
+    }
+
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public void setWidth(float width) {
+        this.width = width;
+    }
+
+    public void setChoice(int choice) {
+        this.choice = choice;
+    }
+
+
+    public float calculateArea() {
+        return length * width;
+    }
+
+    public float calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+
