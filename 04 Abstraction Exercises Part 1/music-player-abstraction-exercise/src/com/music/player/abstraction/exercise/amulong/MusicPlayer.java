2a1868fc1b6d3470c390c68a2349b0425088effd
Kate Ann I. Amulong <amulongkateann@gmail.com>
2024-02-19 19:10:22 +0800

    amulong-music-player-abstraction

--- 04 Abstraction Exercises Part 1/music-player-abstraction-exercise/src/com/music/player/abstraction/exercise/amulong/MusicPlayer.java
+++ 04 Abstraction Exercises Part 1/music-player-abstraction-exercise/src/com/music/player/abstraction/exercise/amulong/MusicPlayer.java
@@ -5,23 +5,19 @@ import java.util.List;
 
 public class MusicPlayer {
     private static List<String> playlist;
-    private List<String> playlist2;
 
     public MusicPlayer() {
         this.playlist = new ArrayList<>();
-        this.playlist2 = new ArrayList<>(this.playlist);
     }
 
     public void addSong(String song) {
         playlist.add(song);
-        playlist2.add(song);
         System.out.println(song + " added to the both playlist.");
     }
 
     public void removeSong(String song) {
         if (playlist.contains(song)) {
             playlist.remove(song);
-            playlist2.remove(song);
             System.out.println(song + " removed from the both playlist.");
         } else {
             System.out.println(song + " is not in the both playlist.");
@@ -33,7 +29,7 @@ public class MusicPlayer {
     }
 
     public void playSong2(int songNumber) {
-        playSong(songNumber, this.playlist2, "BABY MONSTER");
+        playSong(songNumber, this.playlist, "BABY MONSTER");
     }
 
     private void playSong(int songNumber, List<String> playlist, String playlistName) {
