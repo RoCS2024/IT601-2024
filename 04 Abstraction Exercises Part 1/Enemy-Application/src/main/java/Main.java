30dd5d950c007ea8735f0df9c7783de83b8b56f8
zhairanicolepacheco <zhaira.nicole.a.pacheco@gmail.com>
2024-02-20 14:47:22 +0800

    Changes Requested Done

--- 04 Abstraction Exercises Part 1/Enemy-Application/src/main/java/Main.java
+++ 04 Abstraction Exercises Part 1/Enemy-Application/src/main/java/Main.java
@@ -4,17 +4,50 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int attackPoints=10, hitPoints=30;
-        Enemy enemy = new Enemy(attackPoints, hitPoints);
-        int input = 0;
-        int playerHitPoints=50;
+        int enemyAP, enemyHP, input = 0, playerHP;
+        int enemyHpHolder;
 
-        System.out.println("Welcome to the Shooting Game!");
-        System.out.println("Player has " + playerHitPoints + " HitPoints");
-        System.out.println("Enemy has " + hitPoints + " HitPoints");
+        System.out.println("Welcome to the Shooting Game!\n");
+
+        while(true){
+            System.out.print("Enter Player's hit points (HP): ");
+            playerHP = sc.nextInt();
+            if(playerHP > 100 || playerHP == 0) {
+                System.out.println("Invalid input. Please enter numbers from 1 to 100");
+            } else {
+                break;
+            }
+        }
+
+        while(true){
+            System.out.print("Enter Enemy's hit points (HP): ");
+            enemyHP = sc.nextInt();
+            if(enemyHP > Enemy.maxHP || enemyHP == 0) {
+                System.out.println("Invalid input. Please enter numbers from 1 to " + Enemy.maxHP);
+            } else {
+                break;
+            }
+        }
+        enemyHpHolder = enemyHP;
+
+        while(true){
+            System.out.print("Enter Enemy's attack points: ");
+            enemyAP = sc.nextInt();
+            if(enemyAP > Enemy.maxAP ||enemyAP == 0) {
+                System.out.println("Invalid input. Please enter numbers from 1 to " + Enemy.maxAP);
+            } else {
+                break;
+            }
+        }
+        Enemy enemy = new Enemy(enemyAP, enemyHP);
+
+        System.out.println("\nPlayer has " + playerHP + " HP");
+        System.out.println("Enemy has " + enemyHP + " HP");
+        System.out.println("Enemy has " + enemyAP + " Attack Points");
 
         do {
-            System.out.println("\nChoose from the options: ");
+            System.out.println("\nEnemy's POV");
+            System.out.println("Choose from the options: ");
             System.out.println("1. Attack the Player");
             System.out.println("2. Attacked by the Player");
             System.out.println("0. Exit Game");
@@ -23,14 +56,14 @@ public class Main {
                 input = sc.nextInt();
                 switch (input) {
                     case 1: {
-                        if (playerHitPoints > 0) {
-                            playerHitPoints -= attackPoints;
-                            if (playerHitPoints < 0) {
-                                playerHitPoints = 0;
+                        if (playerHP > 0) {
+                            playerHP -= enemyAP;
+                            if (playerHP < 0) {
+                                playerHP = 0;
                             }
                             System.out.println("Player was hit!");
-                            System.out.println("Player Hit Points: " + playerHitPoints);
-                            if (playerHitPoints == 0) {
+                            System.out.println("Player Hit Points: " + playerHP);
+                            if (playerHP == 0) {
                                 System.out.println("Game Over! Player defeated!");
                                 return;
                             }
@@ -40,15 +73,15 @@ public class Main {
                         break;
                     }
                     case 2: {
-                        hitPoints = enemy.receiveHit();
+                        enemyHP = enemy.receiveHit();
                         System.out.println("Enemy was hit!");
-                        System.out.println("Enemy Hitpoints: " + hitPoints);
-                        if (hitPoints <= 0) {
+                        System.out.println("Enemy Hit points: " + enemyHP);
+                        if (enemyHP <= 0) {
                             System.out.println("Enemy defeated!");
                             System.out.println("Spawning a new Enemy.. ");
-                            hitPoints = 30;
-                            enemy = new Enemy(attackPoints, hitPoints);
-                            System.out.println("New Enemy Hitpoints: " + hitPoints);
+                            enemyHP = enemyHpHolder;
+                            enemy = new Enemy(enemyAP, enemyHP);
+                            System.out.println("New Enemy Hit points: " + enemyHP);
                         }
                         break;
                     }
--- 04 Abstraction Exercises Part 1/Enemy-Application/src/main/java/com/shooting/game/enemy/Enemy.java
+++ 04 Abstraction Exercises Part 1/Enemy-Application/src/main/java/com/shooting/game/enemy/Enemy.java
@@ -4,6 +4,9 @@ public class Enemy {
     private int attackPoints;
     private int hitPoints;
 
+    public static int maxAP=20;
+    public static int maxHP=100;
+
     public Enemy(int attackPoints, int hitPoints) {
         this.attackPoints = attackPoints;
         this.hitPoints = hitPoints;
