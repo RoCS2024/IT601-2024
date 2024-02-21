9b3854fb5b44bd8ec7887e11ddcd5f9fb1fe87e9
rkzedrick <ram.kian.zedrick.ramirez@gmail.com>
2024-02-21 20:06:31 +0800

    refactored Cylinder-Volume-Calculator

--- 04 Abstraction Exercises Part 1/cylinder_volume_calculator/src/volume_calculator/CylinderVolCalcu.java
+++ 04 Abstraction Exercises Part 1/cylinder_volume_calculator/src/volume_calculator/CylinderVolCalcu.java
@@ -3,19 +3,18 @@ package volume_calculator;
 public class CylinderVolCalcu {
     private double radius;
     private double height;
-    private double volume;
 
     public CylinderVolCalcu(double radius, double height){
         this.radius = radius;
         this.height = height;
-        this.volume = volume;
     }
 
     private double getProdOfTwo(){
         return this.radius * this.radius * this.height;
     }
     public double getVolumeOfCyl(){
-        final double PI = Math.PI;
+        final double PI = 3.14;
+        double volume;
         return volume = PI * getProdOfTwo();
 
     }
