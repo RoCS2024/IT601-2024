b1d1b176814b75cfc18239ccd877a2036b95f649
rkzedrick <ram.kian.zedrick.ramirez@gmail.com>
2024-02-23 22:44:09 +0800

    Added Reservation-Facility-App Ramirez

---
+++ 05 Inheritance Exercises/Reservation-Facility-App/src/ReserveFacility/Laboratory.java
@@ -0,0 +1,52 @@
+package ReserveFacility;
+import java.util.ArrayList;
+public class Laboratory implements Facility{
+    private ArrayList<Appointment> events = new ArrayList<Appointment>();
+    private int limit = 50;
+    @Override
+    public void addAppointment(Appointment appoint) {
+        events.add(appoint);
+    }
+
+    @Override
+    public void cancelAppointment(String Name,String Date) {
+        for(Appointment event : events){
+            if((event.getName().equals(Name))&&(event.getDate().equals(Date))) {
+                //events.indexOf(event);
+                events.remove(events.indexOf(event));
+                System.out.println("Laboratory Event removed successfully.");
+                return;
+            }
+        }
+
+        System.out.println("Event not found.");
+    }
+
+    @Override
+    public boolean limit(int number) {
+        if(number > limit)
+            return true;
+
+        return false;
+    }
+
+    @Override
+    public Appointment currentEvent(String date) {
+        for(Appointment event : events){
+            if(event.getDate().equals(date)) {
+                return event;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean dateTaken(String date) {
+        for(Appointment event : events){
+            if(event.getDate().equals(date)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
