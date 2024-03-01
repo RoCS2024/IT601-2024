14566749b59cf41cc601eb1c51b7e97eaa6171b6
unknown <kyla.joy.mercader1@gmail.com>
2024-03-01 17:09:03 +0800

    updated-chat-filtering-app

---
+++ 04 Abstraction Exercises Part 1/abstraction-chat-filtering-project/src/main/java/Chat.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Chat {
+    private static Set<String> bannedWords;
+
+    static {
+        bannedWords = new HashSet<>();
+        bannedWords.add("fuck");
+        bannedWords.add("bitch");
+        bannedWords.add("shit");
+        bannedWords.add("asshole");
+        bannedWords.add("cunt");
+        bannedWords.add("bastard");
+        bannedWords.add("whore");
+        bannedWords.add("slot");
+        bannedWords.add("motherfucker");
+    }
+
+    private String senderName;
+    private String recipientName;
+    private List<String> messages;
+
+    private boolean senderOnline;
+
+    public Chat(String senderName, String recipientName) {
+        this.senderName = senderName;
+        this.recipientName = recipientName;
+        this.messages = new ArrayList<>();
+        this.senderOnline = false;
+    }
+
+
+    public boolean isSenderOnline() {
+        return senderOnline;
+    }
+
+
+    public void toggleSenderOnline() {
+        senderOnline = !senderOnline;
+    }
+
+    public void addMessage(String message) {
+        if (!containsBannedWords(message)) {
+            messages.add(message);
+            System.out.println("Message sent successfully.");
+        } else {
+            System.out.println("Message contains banned words and cannot be sent.");
+        }
+    }
+
+    public boolean containsBannedWords(String message) {
+        for (String word : bannedWords) {
+            if (message.toLowerCase().contains(word)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
