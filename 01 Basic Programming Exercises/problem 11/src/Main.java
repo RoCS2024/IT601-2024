369b9dc997de6f286e8f4520efb353ec7b8f7572
Your Name <ferma.ericka.mianne@gmail.com>
2024-04-01 10:23:25 +0800

    basic prog strinf and reverse

--- 01 Basic Programming Exercises/problem 11/src/Main.java
+++ 01 Basic Programming Exercises/problem 11/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int num, reverse = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a Number: ");
+        System.out.print("Enter a number: ");
         num = sc.nextInt();
         int digit;
         while(num !=0){
--- 01 Basic Programming Exercises/problem 13/src/Main.java
+++ 01 Basic Programming Exercises/problem 13/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a String: ");
         String inputString = scanner.nextLine();
 
         int spaceCount = 0;
